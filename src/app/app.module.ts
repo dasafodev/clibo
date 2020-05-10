ec964283638799dc8c419fe73a1ed60f4b6b47b8
Santiago <ds.foreroa@uniandes.edu.co>
2020-05-10 00:13:06 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//Firebase Module
+import { AngularFireModule } from '@angular/fire';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularFireModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyAlvUQhP352K0pJ3wJalS-NgO_NFlhuYZE",
+    authDomain: "ibm-challenge-d1eaa.firebaseapp.com",
+    databaseURL: "https://ibm-challenge-d1eaa.firebaseio.com",
+    projectId: "ibm-challenge-d1eaa",
+    storageBucket: "ibm-challenge-d1eaa.appspot.com",
+    messagingSenderId: "1065922463713",
+    appId: "1:1065922463713:web:23f9ad8e704c692c3df4a0",
+    measurementId: "G-XDNSGRC92B"
+  }
 };
 
 /*
